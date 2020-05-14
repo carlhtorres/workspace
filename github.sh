7e5e70da0ae3f9626e92c847244f7fab8398b4db
Carlos Henrique Torres Silva <carloshenriquetorress@gmail.com>
2020-05-13 21:48:41 -0300

    Script to download my projects from github

---
+++ github.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+git clone git@github.com:carlhtorres/py-twit.git
+git clone git@github.com:carlhtorres/docker-ssh.git
+
+echo "I can be greatly improved, even migrated to Python in the future!"
+echo "Don't leave me to dust!"
