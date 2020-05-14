534d4376ae71628e1f39b22b6cd07550ca7ab304
Carlos Henrique Torres Silva <carloshenriquetorress@gmail.com>
2020-05-13 21:49:10 -0300

    Forgot something

--- github.sh
+++ github.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+git config --global user.email "carloshenriquetorress@gmail.com"
+git config --global user.name "Carlos Henrique Torres Silva"
+
 git clone git@github.com:carlhtorres/py-twit.git
 git clone git@github.com:carlhtorres/docker-ssh.git
 
