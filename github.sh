47aaff3ca120a1439e7b40f262933997f8bc7efa
Carlos Henrique Torres Silva <carloshenriquetorress@gmail.com>
2021-01-14 20:46:19 -0300

    Project list

--- github.sh
+++ github.sh
@@ -7,7 +7,7 @@ git config --global user.name "Carlos Henrique Torres Silva"
 
 # TODO: check if I can use github REST API
 function git_clone() {
-    for prj in $(cat ${PROJECT_HOME}/list)
+    for prj in $(cat projects.list)
     do
         git clone git@github.com:carlhtorres/${prj} ${PROJECT_HOME}/${prj}
     done
@@ -17,7 +17,7 @@ echo "I can be greatly improved, even migrated to Python in the future!"
 echo "Don't leave me to dust!"
 
 function git_sync() {
-    for prj in $(cat ${PROJECT_HOME}/list)
+    for prj in $(cat projects.list)
     do
         cd ${PROJECT_HOME}/${prj}
         git push origin master
